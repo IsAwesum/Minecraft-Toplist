812e48f63219b5d3a4d5a67052dddee24662eec4
Murph <>
2012-07-26 16:33:11 +0100

    Votifier notification basics. Still requires work.

--- application/libraries/VotifierNotification.php
+++ application/libraries/VotifierNotification.php
@@ -1,13 +1,43 @@
 <?php
-/**
- * Sends a Votifier notification to the server when a vote is
- * received by the website.
- *
- * @author Conor Murphy
- */
+ 
+error_reporting(E_ALL);
+ 
+$player = $_POST['playerName'];
+$ip = $_SERVER['REMOTE_ADDR'];
+ 
 
-class VotifierNotification {
-    
+// Details of the vote.
+$str = "VOTE\n" .
+       $siteName."\n" .
+       $player."\n";
+       $ip."\n";
+       time()."\n";
+
+$leftover = (256 - strlen($str)) / 2;
+ 
+while ($leftover > 0) {
+    $str .= "\x0";
+    $leftover--;
 }
+ 
+
+$key = "";
+$key = wordwrap($key, 65, "\n", true);
+$key = <<<EOF
+-----BEGIN PUBLIC KEY-----
+$key
+-----END PUBLIC KEY-----
+EOF;
+
+openssl_public_encrypt($str, $encrypted, $key);
+
+$socket = fsockopen($host, $votifierPort, $errno, $errstr, 15);
+ 
+if (!$socket) {
+    die("Failed to connect to Votifier.");
+}
+
+fwrite($socket, $encrypted);
+echo "Thanks for voting! You can vote again in 24 hours.";
 
 ?>
