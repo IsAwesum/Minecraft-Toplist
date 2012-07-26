83c97e0760ab8e489aa206cb318ef0133e6a8e1a
Murph <>
2012-07-26 14:52:13 +0100

    Work from this file.

---
+++ application/libraries/VotifierNotification.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Sends a Votifier notification to the server when a vote is
+ * received by the website.
+ *
+ * @author Conor Murphy
+ */
+
+class VotifierNotification {
+    
+}
+
+?>
